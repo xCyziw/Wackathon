144214ea06ee4103dbf76d1a6db14ef3b75789d9
Olivia Tang <tango2@uw.edu>
2020-01-18 19:08:12 -0800

    yeah

--- src/components/NewsLetter/index.js
+++ src/components/NewsLetter/index.js
@@ -13,15 +13,11 @@ export class NewsInfo extends React.Component{
                 <h1><span className="purple">Be the change</span>, donate to women in need blah blah. </h1>
                 <div>
                     <p className='body22'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Maecenas mattis volutpat felis vitae dignissim. Nullam eget 
-                        quam dui. Sed sit amet odio lacinia, malesuada justo vitae, 
-                        sollicitudin massa. Nam pretium vestibulum sollicitudin. Curabitur
-                        lobortis at dolor id porta. Aliquam lobortis erat nibh, nec
-                        porta dui fringilla in.
+                        Sign up for our weekly newsletter to keep up with current <b>Uplift(ing)</b> events. By subscribing to the Uplift newsletter we will update you on our journey 
+                        helping homeless women in need in the Seattle Area. Subscribe and we will inform you on opportunities to help you get involved.
                     </p>
                     <p className='body22'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis volutpat felis vitae dignissim. Nullam eget quam dui.
+                        Uplift is a local nonprofit that creates warm and welcoming homes for women and children coming out of homelessness.
                     </p>
                 </div>
                 <button className='volunteer'>Volunteer</button>
