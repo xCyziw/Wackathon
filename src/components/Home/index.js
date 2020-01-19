79bf69906c2b450cc62e6a33c45389f30e998f69
Lawrence Fiesta <lfiesta86@gmail.com>
2020-01-18 18:53:38 -0800

    pulliung

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -21,8 +21,8 @@ class index extends Component {
                             <img src={Woman} alt="woman" className="woman" />
                             <img src={Family} alt="family" className="family" />
                         </div>
-                        <Mission />
-                        <Impact />
+                        {/* <Mission />
+                        <Impact /> */}
 
                     </div>
                 </div>
