7f1684c28e8695869568113fb010bb726c117e2b
xCyziw <neonlight003@gmail.com>
2020-01-18 18:42:23 -0800

    style changes

--- src/components/Impact/index.js
+++ src/components/Impact/index.js
@@ -10,7 +10,7 @@ class index extends Component {
             <div className="impact-container">
                 <div className="impact">
                     <div className="impactRow">
-                        <h1>[Vision statement here blahablahdsaklfslfj alkfjaslfjlsajfljslf jlajflajf]</h1>
+                        <h1>Our impact blah blah</h1>
                         <div className="line"></div>
                     </div>
                     
