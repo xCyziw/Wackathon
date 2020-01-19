4d5df900884d0a10ec9dbce1cb8f5c58d6c4855e
Lawrence Fiesta <lfiesta86@gmail.com>
2020-01-18 17:43:59 -0800

    pulling changes

--- src/pages/DropOff/index.js
+++ src/pages/DropOff/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Input from '@material-ui/core/Input';
 import API from "../../utils/API";
+import './style.css';
 export default class DropOff extends Component {
 
   state = {
@@ -39,17 +40,49 @@ export default class DropOff extends Component {
   classes = this.useStyles;
   render() {
     return (
-      <div>
-        <h1>Drop Off Furniture Information</h1>
-        <p>Lorem ipsum</p>
+      <>
+
+
+
+      <section className="container">
+        <section className="info">
+          <h1>Drop Off Furniture Information</h1>
+          <p>Lorem ipsum dolor sit amet,</p>
+        </section>
+        
         <form className={this.classes.root} noValidate autoComplete="off">
-          <Input placeholder="First Name" inputProps={{ 'aria-label': 'description' }} />
-          <Input placeholder="Last Name" inputProps={{ 'aria-label': 'description' }} />
-          <Input placeholder="Furniture type" inputProps={{ 'aria-label': 'description' }} />
-          <input type='file' id='images' placeholder='images' multiple onChange={this.uploadFile} />
-          <Input placeholder="Drop Off Location" inputProps={{ 'aria-label': 'description' }} />
+          <section className="firstName">
+            <label>
+              First Name
+            </label><br></br>
+            <Input placeholder="First Name" inputProps={{ 'aria-label': 'description' }} />
+          </section>
+
+          <section className='lastName'>
+            <label>
+              Last Name
+            </label><br></br>
+            <Input placeholder="Last Name" inputProps={{ 'aria-label': 'description' }} />
+          </section>
+
+          <section className='furniture'>
+            <label>Furniture type</label>
+            <br></br>
+            <Input placeholder="Furniture type" inputProps={{ 'aria-label': 'description' }} />
+          </section>
+          
+          <section className='picture'>
+            <label>Picture</label><br></br>
+            <input className="btn" type='file' id='images' placeholder='images' multiple onChange={this.uploadFile} />
+          </section>
+
+          <section className='drop'>
+            <label>Drop off Location</label>
+            <Input placeholder="Drop Off Location" inputProps={{ 'aria-label': 'description' }} />
+          </section>
         </form>
-      </div>
+      </section>
+      </>
     )
   }
 }
